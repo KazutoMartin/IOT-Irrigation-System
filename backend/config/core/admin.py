6e32c3d64bdc796ada295f381d5e051e887605e9
Matin <kazutomartin20@gmail.com>
2026-01-01 16:02:07 +0330

    add models to admin

--- backend/config/core/admin.py
+++ backend/config/core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import HumidityRecord, PumpState
+from .models import HumidityRecord, PumpState, Device, ThresholdConfig
 
 # Register your models here.
 admin.site.register(HumidityRecord)
 admin.site.register(PumpState)
+admin.site.register(Device)
+admin.site.register(ThresholdConfig)
